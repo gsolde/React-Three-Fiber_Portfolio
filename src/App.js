f3292d1a67ae147a58823d34cb7562fd45fac356
gsolde <gsolde@gmail.com>
2021-03-31 17:02:40 +0200

    feat: set perspective camera position

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import Carousel from "./containers/carousel";
 
 function App() {
   let tiles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+  function Camera() {
+    useThree(({ camera }) => {
+      camera.position.x = 0;
+      camera.position.y = 0;
+      camera.position.z = -15;
+    });
+    return null;
+  }
+
   return (
     <Canvas>
+      <Camera />
       <Carousel activities={tiles} position={[0, 0, 0]} />
     </Canvas>
   );
