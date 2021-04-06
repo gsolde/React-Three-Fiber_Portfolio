416b7d5b45d84b2ba128a1559cb2e0c9fa90ac53
gsolde <gsolde@gmail.com>
2021-04-06 20:03:07 +0200

    feat: add camera controller

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Canvas } from "react-three-fiber";
 import Carousel from "./containers/carousel";
 import Lights from "./components/lights";
+import CameraController from "./components/cameraController";
 
 function App() {
   let tiles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
   return (
-    <Canvas shadowMap camera={{ position: [0, 0, 5], fov: 90 }}>
+    <Canvas shadowMap>
+      <CameraController />
       <directionalLight />
       <Carousel activities={tiles} position={[0, 0, 19]} />
       <Lights />
---
+++ src/components/cameraController.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useThree } from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const CameraController = () => {
+  const { camera, gl } = useThree();
+  useEffect(() => {
+    const controls = new OrbitControls(camera, gl.domElement);
+    controls.minDistance = 4.5;
+    controls.maxDistance = 8;
+    return () => {
+      controls.dispose();
+    };
+  }, [camera, gl]);
+  return null;
+};
+
+export default CameraController;
