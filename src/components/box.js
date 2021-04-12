ec3b1d116e6b5b96777b34e0bd5cb012b4b99b6b
gsolde <gsolde@gmail.com>
2021-04-13 00:59:34 +0200

    fix: naming

--- src/components/box.js
+++ src/components/box.js
@@ -4,44 +4,44 @@ import { useFrame } from "react-three-fiber";
 import * as THREE from "three";
 
 const Box = (props) => {
-  let tile = useRef();
-  let tileRotationY;
-  let tileTargetRotationY;
-  let tileScaleZ;
-  let tileTargetScaleZ;
-  let tilePositionZ;
-  let tileTargetPositionZ;
+  let box = useRef();
+  let boxRotationY;
+  let boxTargetRotationY;
+  let boxScaleZ;
+  let boxTargetScaleZ;
+  let boxPositionZ;
+  let boxTargetPositionZ;
 
   function handlePointerOver() {
-    tileRotationY = tile.current.rotation.y;
-    tileTargetRotationY = tileRotationY + Math.PI;
-    tileScaleZ = tile.current.scale.z;
-    tileTargetScaleZ = 5;
-    tilePositionZ = tile.current.position.z;
-    tileTargetPositionZ = tilePositionZ - 1.5;
+    boxRotationY = box.current.rotation.y;
+    boxTargetRotationY = boxRotationY + Math.PI;
+    boxScaleZ = box.current.scale.z;
+    boxTargetScaleZ = 5;
+    boxPositionZ = box.current.position.z;
+    boxTargetPositionZ = boxPositionZ - 1.5;
   }
 
   function handlePointerOut() {
-    tileScaleZ = tile.current.scale.z;
-    tileTargetScaleZ = 1;
+    boxScaleZ = box.current.scale.z;
+    boxTargetScaleZ = 1;
   }
 
   useFrame(() => {
-    if (tileScaleZ) {
-      tileScaleZ = THREE.MathUtils.lerp(tileScaleZ, tileTargetScaleZ, 0.1);
-      tile.current.scale.set(1, 1, tileScaleZ);
-      tilePositionZ = THREE.MathUtils.lerp(tilePositionZ, tileTargetPositionZ, 0.1);
-      tile.current.position.set(tile.current.position.x, tile.current.position.y, tilePositionZ);
-      tileRotationY = THREE.MathUtils.lerp(tileRotationY, tileTargetRotationY, 0.1);
-      tile.current.rotation.set(0, tileRotationY, 0);
+    if (boxScaleZ) {
+      boxScaleZ = THREE.MathUtils.lerp(boxScaleZ, boxTargetScaleZ, 0.1);
+      box.current.scale.set(1, 1, boxScaleZ);
+      boxPositionZ = THREE.MathUtils.lerp(boxPositionZ, boxTargetPositionZ, 0.1);
+      box.current.position.set(box.current.position.x, box.current.position.y, boxPositionZ);
+      boxRotationY = THREE.MathUtils.lerp(boxRotationY, boxTargetRotationY, 0.1);
+      box.current.rotation.set(0, boxRotationY, 0);
     }
   });
 
   return (
     <group>
       <RoundedBox
-        ref={tile}
-        position={props.tilePosition}
+        ref={box}
+        position={props.boxPosition}
         args={[0.95, 0.95, 0.5]}
         radius={0.1}
         smoothness={10}
