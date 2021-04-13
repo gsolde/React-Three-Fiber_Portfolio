42c4d992a3b1fb475865e77f7e961aaff64afc6c
gsolde <gsolde@gmail.com>
2021-04-13 19:08:47 +0200

    feat: box animation

--- src/components/box.js
+++ src/components/box.js
@@ -31,7 +31,7 @@ const Box = (props) => {
       boxScaleZ = box.current.scale.z;
       boxTargetScaleZ = 5;
       boxPositionZ = box.current.position.z;
-      boxTargetPositionZ = boxPositionZ - 1.5;
+      boxTargetPositionZ = Math.random() > 0.5 ? boxPositionZ - 1.5 : boxPositionZ + 1.5;
     }
   }
 
